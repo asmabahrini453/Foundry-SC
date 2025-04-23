035a63353cf3647570ddcf70caa70df4cf2f553d
asmabahrini453 <bahriniasma453@gmail.com>
2025-04-23 19:59:54 +0200

    trying to deploy sc with script

---
+++ foundry-simple-storage/script/DeploySimpleStorage.s.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+//we need to tell forge that this is  a script
+//we need to import the script interface
+import "forge-std/Script.sol";
+contract DeploySimpleStorage {
+
+}
