93574918954303c0c808c5734437f046f5f795d6
asmabahrini453 <bahriniasma453@gmail.com>
2025-04-23 20:27:18 +0200

    deploying the SimpleStorageContract with a script

--- foundry-simple-storage/script/DeploySimpleStorage.s.sol
+++ foundry-simple-storage/script/DeploySimpleStorage.s.sol
@@ -3,7 +3,23 @@
 pragma solidity 0.8.19;
 //we need to tell forge that this is  a script
 //we need to import the script interface
-import "forge-std/Script.sol";
-contract DeploySimpleStorage {
+
+import {Script} from "forge-std/Script.sol";
+import {SimpleStorage} from "../src/SimpleStorage.sol";
+
+contract DeploySimpleStorage is Script{  
+    //main func that gets called to deploy our contract
+    function run() external returns (SimpleStorage) {
+        //vm is only used in Foundry. (you can find it in docs/sheetcodes)
+        //start broadcasting to the rpc , so everything (tx) inside startBroadcasting and stopBroadcasting is going to be sent
+        //to the RPC and get deployed. 
+        vm.startBroadcast();
+        //deploy the contract
+        SimpleStorage simpleStorage = new SimpleStorage(); //instantiate SC
+        //stop broadcasting
+        vm.stopBroadcast();
+        //return the deployed contract
+        return simpleStorage;
+    }
 
 }
