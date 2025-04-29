995c8749939769725040ad2276c4d150bb013e00
asmabahrini453 <bahriniasma453@gmail.Com>
2025-04-29 12:59:54 +0200

    optimizing code to have more gas efficiency

--- foundry-fund-me/src/FundMe.sol
+++ foundry-fund-me/src/FundMe.sol
@@ -2,27 +2,12 @@
 pragma solidity ^0.8.18;
 
 // Note: The AggregatorV3Interface might be at a different location than what was in the video!
-// import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {AggregatorV3Interface} from "lib/chainlink-brownie-contracts/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 import {PriceConverter} from "./PriceConverter.sol";
-
-interface AggregatorV3Interface {
-  function decimals() external view returns (uint8);
-
-  function description() external view returns (string memory);
-
-  function version() external view returns (uint256);
-
-  function getRoundData(
-    uint80 _roundId
-  ) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
-
-  function latestRoundData()
-    external
-    view
-    returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
-}
+//Define custom error 
 error NotOwner();
+
 //this project is about funding a SC: sending ETH to a contract in exchange for a reward
 //Steps are: 1/ Get funds from users 2/ Withdraw funds 3/ Set a minimum funding value in USD
 contract FundMe {
@@ -32,54 +17,88 @@ contract FundMe {
 
     address[] public funders; //<-- state variable
 
-    // Could we make this constant?  /* hint: no! We should make it immutable! */
-    address public /* immutable */ i_owner;
-     
-    uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
+    //constant,immutable are values that are fixed at compile time and connot change
 
+    address public immutable i_owner; //immutable: we can only assign it once, at the time of deployment
+
+    uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
+     //when u add "constatnt" the variable doesn't take up space in the blockchain -->No Storage spot( the compiler hardcodes the literal value directly into the contract's bytecode.)
+     // -->easier to read
+
+    //assign an address to the owner of the contract at the time of deployment
+    //constructor will be called the moment we deploy the contract
     constructor() {
         i_owner = msg.sender;
     }
 
-//allow all users tosend ETH to the contract 
-//payable : is a keyword that allows the function to receive ETH
-//msg.value:to access the amount of ETH sent to the contract
-//require: to spend at least a minimum amount of ETH
+    //allow all users tosend ETH to the contract
+    //payable : is a keyword that allows the function to receive ETH
+    //msg.value:to access the amount of ETH sent to the contract
+    //require: to spend at least a minimum amount of ETH
     function fund() public payable {
-        require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");
+        require(
+            msg.value.getConversionRate() >= MINIMUM_USD,
+            "You need to spend more ETH!"
+        );
         // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
         addressToAmountFunded[msg.sender] += msg.value;
         funders.push(msg.sender);
     }
 
     function getVersion() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
         return priceFeed.version();
     }
 
+    //modifiers: are reusable code blocks that can be used to change or add conditions to the behavior of functions, often for access control or validations.
     modifier onlyOwner() {
         // require(msg.sender == owner);
+        //custom errors are more gas efficient
         if (msg.sender != i_owner) revert NotOwner();
-        _;
+        _; //this refering to executing the code later than the modifier code
+        //if we put it before the if then we execute the code before the modifier code
     }
 
+    //only the owner can withdraw ETH
+    //execute the modifier "onlyOwner" first then the   _; then everything else
     function withdraw() public onlyOwner {
-        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
             address funder = funders[funderIndex];
-            addressToAmountFunded[funder] = 0;
+            addressToAmountFunded[funder] = 0; //reset what they funded in fund() back to 0
         }
-        funders = new address[](0);
-        // // transfer
+        funders = new address[](0); //reset the funders array to 0
+        // Now, withdraw the funds : send eth back to the sender
+        // // way1: transfer
+        // // transfer is a function that sends ether to an address
+        //.balance :returns the some of money in ETH
+        //payable(msg.sender) = payable address of the sender : in solidity, to send the native currency (ETH) to an address, we need to convert it to a payable address
+        //issue with transfer: it only sends 2300 gas, which is not enough for some contracts to execute otherwise it throws an error
         // payable(msg.sender).transfer(address(this).balance);
 
-        // // send
+        // //way2:  send
+        // issue with send: it only sends 2300 gas and returns a boolean value indicating whether the transfer was successful or not. If it fails, it doesn't throw an error, but it doesn't send the ether either.
         // bool sendSuccess = payable(msg.sender).send(address(this).balance);
         // require(sendSuccess, "Send failed");
 
-        // call
-        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        //way3: call :the recommanded way to send native ETH to an address
+        //with call we can send all the gas to the recipient and it returns a boolean value indicating whether the transfer was successful or not. If it fails, it throws an error.
+        // call is a low-level fct, we can use it to call any fct in ethereum without the need to an ABI
+        //call forward all gas or set gas
+        //("") is empty because we're not calling any fct in the recipient contract, we're just sending ether , so using call to send TX
+        //call returns 2 variables a (bool callSuccess,) and b (bytes memory dataReturned)
+        //if we use call to call a fct that returns a value we can use the second variable to get the data returned
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
         require(callSuccess, "Call failed");
     }
+
     // Explainer from: https://solidity-by-example.org/fallback/
     // Ether is sent to contract
     //      is msg.data empty?
@@ -92,20 +111,13 @@ contract FundMe {
     //  /        \
     //receive()  fallback()
 
+//In Solidity, the receive() function handles plain Ether transfers with no data,
+// while the fallback() function is called when a non-existent function is invoked or when Ether is sent with data.
     fallback() external payable {
         fund();
     }
-
     receive() external payable {
         fund();
     }
 }
 
-// Concepts we didn't cover yet (will cover in later sections)
-// 1. Enum
-// 2. Events
-// 3. Try / Catch
-// 4. Function Selector
-// 5. abi.encode / decode
-// 6. Hash with keccak256
-// 7. Yul / Assembly
--- foundry-fund-me/src/PriceConverter.sol
+++ foundry-fund-me/src/PriceConverter.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {AggregatorV3Interface} from 
+"lib/chainlink-brownie-contracts/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol"; 
 
 
 library PriceConverter {
